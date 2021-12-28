fdcc41f91bbbd888db60fe3b62e2f0818bd3dc58
Stefan <user>
2021-12-28 14:35:04 +0100

    kreirana desktop aplikacija

---
+++ DesktopAplikacija/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace DesktopAplikacija
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnKarta = new System.Windows.Forms.Button();
+            this.btnGledalac = new System.Windows.Forms.Button();
+            this.btnFilm = new System.Windows.Forms.Button();
+            this.btnZarada = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 91F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(933, 568);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.btnKarta, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.btnGledalac, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.btnFilm, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this.btnZarada, 0, 4);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 8;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090909F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090909F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090909F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090909F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090909F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090909F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090909F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090909F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(77, 562);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // btnKarta
+            // 
+            this.btnKarta.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnKarta.Image = ((System.Drawing.Image)(resources.GetObject("btnKarta.Image")));
+            this.btnKarta.Location = new System.Drawing.Point(3, 73);
+            this.btnKarta.Name = "btnKarta";
+            this.btnKarta.Size = new System.Drawing.Size(71, 64);
+            this.btnKarta.TabIndex = 0;
+            this.btnKarta.UseVisualStyleBackColor = true;
+            // 
+            // btnGledalac
+            // 
+            this.btnGledalac.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnGledalac.Image = ((System.Drawing.Image)(resources.GetObject("btnGledalac.Image")));
+            this.btnGledalac.Location = new System.Drawing.Point(3, 143);
+            this.btnGledalac.Name = "btnGledalac";
+            this.btnGledalac.Size = new System.Drawing.Size(71, 64);
+            this.btnGledalac.TabIndex = 1;
+            this.btnGledalac.UseVisualStyleBackColor = true;
+            // 
+            // btnFilm
+            // 
+            this.btnFilm.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnFilm.Image = ((System.Drawing.Image)(resources.GetObject("btnFilm.Image")));
+            this.btnFilm.Location = new System.Drawing.Point(3, 213);
+            this.btnFilm.Name = "btnFilm";
+            this.btnFilm.Size = new System.Drawing.Size(71, 64);
+            this.btnFilm.TabIndex = 2;
+            this.btnFilm.UseVisualStyleBackColor = true;
+            // 
+            // btnZarada
+            // 
+            this.btnZarada.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnZarada.Image = ((System.Drawing.Image)(resources.GetObject("btnZarada.Image")));
+            this.btnZarada.Location = new System.Drawing.Point(3, 283);
+            this.btnZarada.Name = "btnZarada";
+            this.btnZarada.Size = new System.Drawing.Size(71, 64);
+            this.btnZarada.TabIndex = 3;
+            this.btnZarada.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 568);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button btnKarta;
+        private System.Windows.Forms.Button btnGledalac;
+        private System.Windows.Forms.Button btnFilm;
+        private System.Windows.Forms.Button btnZarada;
+    }
+}
+
